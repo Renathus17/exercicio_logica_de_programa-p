5de86cce3eb98409405e9fd0601dad8cd8f35583
Renathus17 <renato.gr143@gmail.com>
2024-10-29 22:27:39 -0300

    Delete Aula2910/Exercicio directory

--- Aula2910/Exercicio/ex5/exe5.js
+++
@@ -1,6 +0,0 @@
-//Faça um Programa que converta metros para centímetros.
-
-var a = parseFloat(prompt("Digite o valor do metro:  "));
-res = a*100
- 
-console.log("O metro convertido em centimetros é: ", res);
